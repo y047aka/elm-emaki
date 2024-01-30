d1b9ca5386c81d3b8c610873b2343e7e7c01242c
Yoshitaka Totsuka <y047aka@users.noreply.github.com>
2024-01-30 10:52:22 +0900

    env: elm-css-palette v3 (#26)

--- package/src/DesignToken/Color.elm
+++ package/src/DesignToken/Color.elm
@@ -12,69 +12,69 @@ module DesignToken.Color exposing
 
 -}
 
-import Css exposing (Color, hsl)
+import Css.Color exposing (Hsl360, hsl)
 
 
-white : Color
+white : Hsl360
 white =
     hsl 0 0 1
 
 
-black : Color
+black : Hsl360
 black =
     hsl 0 0 0
 
 
-grey020 : Color
+grey020 : Hsl360
 grey020 =
     hsl 0 0 0.2
 
 
-grey030 : Color
+grey030 : Hsl360
 grey030 =
     hsl 0 0 0.3
 
 
-grey050 : Color
+grey050 : Hsl360
 grey050 =
     hsl 0 0 0.5
 
 
-grey060 : Color
+grey060 : Hsl360
 grey060 =
     hsl 0 0 0.6
 
 
-grey070 : Color
+grey070 : Hsl360
 grey070 =
     hsl 0 0 0.7
 
 
-grey085 : Color
+grey085 : Hsl360
 grey085 =
     hsl 0 0 0.85
 
 
-grey090 : Color
+grey090 : Hsl360
 grey090 =
     hsl 0 0 0.9
 
 
-grey095 : Color
+grey095 : Hsl360
 grey095 =
     hsl 0 0 0.95
 
 
-red050 : Color
+red050 : Hsl360
 red050 =
     hsl 0 1 0.5
 
 
-green050 : Color
+green050 : Hsl360
 green050 =
     hsl 120 1 0.5
 
 
-blue050 : Color
+blue050 : Hsl360
 blue050 =
     hsl 240 1 0.5
--- package/src/DesignToken/Palette.elm
+++ package/src/DesignToken/Palette.elm
@@ -15,13 +15,14 @@ module DesignToken.Palette exposing
 
 -}
 
-import Css exposing (Color, Style, hover, hsla)
+import Css exposing (Style, hover)
+import Css.Color exposing (Hsl360, hsla)
 import Css.Palette exposing (Palette, init, setBackground, setColor)
 import Css.Palette.Extra exposing (light_dark)
 import DesignToken.Color exposing (black, grey020, grey030, grey060, grey070, grey085, grey090, grey095, white)
 
 
-light : Palette
+light : Palette Hsl360
 light =
     { background = Just white
     , color = Just grey030
@@ -29,7 +30,7 @@ light =
     }
 
 
-dark : Palette
+dark : Palette Hsl360
 dark =
     { background = Just black
     , color = Just grey095
@@ -37,7 +38,7 @@ dark =
     }
 
 
-textOptional : Palette
+textOptional : Palette Hsl360
 textOptional =
     { background = Nothing
     , color = Just grey060
@@ -49,7 +50,7 @@ textOptional =
 -- NAVIGATION
 
 
-navigation : Bool -> Palette
+navigation : Bool -> Palette Hsl360
 navigation isDarkMode =
     light_dark isDarkMode
         { light =
@@ -65,7 +66,7 @@ navigation isDarkMode =
         }
 
 
-navItem : Bool -> ( Palette, List ( List Style -> Style, Palette ) )
+navItem : Bool -> ( Palette Hsl360, List ( List Style -> Style, Palette Hsl360 ) )
 navItem isDarkMode =
     light_dark isDarkMode
         { light =
@@ -79,7 +80,7 @@ navItem isDarkMode =
         }
 
 
-navItemSelected : Bool -> Palette
+navItemSelected : Bool -> Palette Hsl360
 navItemSelected isDarkMode =
     light_dark isDarkMode
         { light =
@@ -97,7 +98,7 @@ navItemSelected isDarkMode =
 -- PLAYGROUND
 
 
-playground : Bool -> Palette
+playground : Bool -> Palette Hsl360
 playground isDarkMode =
     light_dark isDarkMode
         { light =
@@ -113,7 +114,7 @@ playground isDarkMode =
         }
 
 
-propsPanel : Bool -> Palette
+propsPanel : Bool -> Palette Hsl360
 propsPanel isDarkMode =
     light_dark isDarkMode
         { light = { init | background = dark.background |> Maybe.map (setAlpha_fixme 0.1) }
@@ -121,7 +122,7 @@ propsPanel isDarkMode =
         }
 
 
-propsField : Bool -> Palette
+propsField : Bool -> Palette Hsl360
 propsField isDarkMode =
     light_dark isDarkMode
         { light = { init | background = light.background |> Maybe.map (setAlpha_fixme 0.7) }
@@ -129,7 +130,7 @@ propsField isDarkMode =
         }
 
 
-formField : Palette
+formField : Palette Hsl360
 formField =
     { light | background = Just (hsla 0 0 0 0) }
 
@@ -138,6 +139,6 @@ formField =
 -- HELPERS
 
 
-setAlpha_fixme : Float -> Color -> Color
-setAlpha_fixme alpha { red, green, blue } =
-    Css.rgba (red * 255) (green * 255) (blue * 255) alpha
+setAlpha_fixme : Float -> Hsl360 -> Hsl360
+setAlpha_fixme alpha { hue, saturation, lightness } =
+    hsla hue saturation lightness alpha
